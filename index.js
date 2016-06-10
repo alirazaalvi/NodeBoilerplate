149b7907a6acf3168bffaf30719527879b6ce07a
Ali Raza Alvi <aliraza432@gmail.com>
2016-06-10 20:14:11 +0500

    Added views support using handlebars templating engine.

--- index.js
+++ index.js
@@ -6,51 +6,61 @@ const Good = require('good');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+
+server.connection({
+  host: 'localhost',
+  port: 8000
 });
 
 
 server.register(
     [
-        require('inert'), 
-        {
-            register: Good,
-            options: {
-                reporters: [{
-                    reporter: require('good-console'),
-                    events: {
-                        response: '*',
-                        log: '*'
-                    }
-                }]
+      require('inert'),
+      {
+        register: Good,
+        options: {
+          reporters: [{
+            reporter: require('good-console'),
+            events: {
+              response: '*',
+              log: '*'
             }
+          }]
         }
-    ]
-
-    , (err) => {
-
-    if (err) {
+      },
+      require('vision')
+    ], (err) => {
+      if (err) {
         throw err;
-    }
+      }
 
-    server.log('info', 'Server running at: ' + server.info.uri);
+      server.log('info', 'Server running at: ' + server.info.uri);
 
-    server.route({
+      server.route({
         method: 'GET',
         path: '/',
         handler: function (request, reply) {
-            return reply.file('./views/index.html');
+          reply.view('index');
         }
+      });
     });
+
+//setting the templating engine
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  relativeTo: __dirname,
+  path: './views',
+  layoutPath: './view/layout',
+  helpersPath: './views/helpers'
 });
 
 // Start the server
 server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 });
