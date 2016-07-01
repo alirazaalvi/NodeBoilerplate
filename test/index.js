16425d4ddee1065b769ba3bcbb8eea584a6824ab
Ali Raza Alvi <aliraza432@gmail.com>
2016-07-01 17:18:25 +0400

    Added test for 404.

--- test/index.js
+++ test/index.js
@@ -5,18 +5,31 @@ const server = require('../index');
 
 var lab = exports.lab = Lab.script();
 
-lab.experiment("Server", function() {
-  lab.test("/", function(done) {
+lab.experiment("Server", function () {
+  lab.test("index page", function (done) {
     var options = {
       method: "GET",
       url: "/"
     };
 
-    server.inject(options, function(response) {
+    server.inject(options, function (response) {
       Code.expect(response.statusCode).to.equal(200);
       done();
     });
   });
+
+  lab.test("opening the non existent page", function (done) {
+    var options = {
+      method: "GET",
+      url: "/non-existent-page"
+    };
+
+    server.inject(options, function (response) {
+      Code.expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
 });
 
 
