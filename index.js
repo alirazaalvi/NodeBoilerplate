81ee1f85b3ca0db03b6eb90091fc1693787f311d
Ali Raza Alvi <aliraza432@gmail.com>
2016-07-08 03:14:40 +0400

    Added babel support to lab tests.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-'use strict';
-const Hapi = require('hapi');
+import Hapi from 'hapi';
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -31,22 +30,20 @@ server.register([{
 
   server.log('info', 'Server running at: ' + server.info.uri);
 
-  server.route({
+  server.route([{
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
       reply.view('index');
     }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/api',
-    handler: function (request, reply) {
-      reply({result: 'success'});
-    }
-  });
-
+  },
+    {
+      method: 'GET',
+      path: '/api',
+      handler: function (request, reply) {
+        reply({result: 'success'});
+      }
+    }]);
 });
 
 //setting the templating engine
